5ac980e22d6528e977168e1eb7a4b7f23a3a3df3
Raphael Adamski <raphaeladamski@hotmail.com>
2018-05-26 11:53:59 -0300

    Resize and crop images for keras inception v3

--- main.py
+++ main.py
@@ -5,14 +5,61 @@ import csv
 import pickle
 import re
 from time import time
+
 import numpy as np
+import cv2
 
 from sklearn import  model_selection
 
 from joblib import Parallel, delayed
 import multiprocessing
 
+def centered_crop(img, new_height=299, new_width=299):
+    width = np.size(img, 1)
+    height = np.size(img, 0)
 
+    left = (width - new_width) // 2
+    top = (height - new_height) // 2
+    right = (width + new_width) // 2
+    bottom = (height + new_height) // 2
+    c_img = img[top:bottom, left:right, :]
+    return c_img
+
+
+def resize(image, size=299, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    width = None
+    height = None
+    (h, w) = image.shape[:2]
+
+    if h > w:
+        width = size
+    else:
+        height = size
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+start_time = time()
 parser = argparse.ArgumentParser(description='Dog breed classifier')
 parser.add_argument('-s', '--size', type=int, help='Train Size to use', default=-1)
 parser.add_argument('-i', '--input-train', type=str, help='Path to files containing the train dataset',
@@ -55,3 +102,13 @@ else:
     y_train = train_classes
     x_test = val_image_path
     y_test = val_classes
+
+images = []
+for path in x_train:
+    img = cv2.imread(path)
+    img = resize(img, 299)
+    img = centered_crop(img, 299, 299)
+    images.append(img)
+
+
+print("--- %s seconds ---" % (time() - start_time))
