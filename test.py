deecaff8cec03931ba3a423c063331995557223b
vrvitor <vitor@vista.aero>
2018-06-04 09:12:55 -0300

    Added pickle_safe

--- test.py
+++ test.py
@@ -21,6 +21,35 @@ environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from joblib import Parallel, delayed
 import multiprocessing
 
+def resize(image, size=299, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    width = None
+    height = None
+    (h, w) = image.shape[:2]
+    if h > w:
+        width = size
+    elif h < w:
+        height = size
+    # check to see if the width is None
+    if height is not None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    # otherwise, the height is None
+    elif width is not None:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+    else:
+        dim = (size, size)
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+    # return the resized image
+    return resized
 
 start_time = time()
 parser = argparse.ArgumentParser(description='Dog breed classifier')
@@ -63,19 +92,19 @@ for clazz in listdir(TEST):
 batch_size = 16
 
 test_datagen = preprocessing.image.ImageDataGenerator(
-    #preprocessing_function=applications.inception_v3.preprocess_input
+    rescale=1./255
 )
 
 test_generator = test_datagen.flow_from_directory(
     TEST,
-    classes=["{:02d}".format(x) for x in range(NUM_CLASSES)],
+    classes=["{:02d}".format(x) for x in range(83)],
     target_size=(299, 299),
     batch_size=1,
     shuffle=False,
     class_mode='categorical'
 )
 
-model_name = "inceptionv3.h5"
+model_name = "inceptionv3"
 
 try:
     model = models.load_model(model_name + ".h5")
@@ -83,16 +112,16 @@ try:
 except OSError:
     pass
 
-score = model.evaluate_generator(test_generator, verbose=1, use_multiprocessing=True)
+score = model.evaluate_generator(test_generator, verbose=1, steps=len(test_classes), use_multiprocessing=True)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 test_generator.class_mode = None
 
-prob = model.predict_generator(test_generator, verbose=1, workers=1, steps=len(test_classes))
+prob = model.predict_generator(test_generator, verbose=1, workers=1, steps=len(test_classes), pickle_safe=True)
 
 Y_pred = np.argmax(prob, axis=1)
 accuracy = (len(test_classes) - np.count_nonzero(Y_pred - test_classes) + 0.0)/len(test_classes)
-print("Accuracy on testidation set of %d samples: %f" % (len(test_classes), accuracy))
+print("Accuracy on test set of %d samples: %f" % (len(test_classes), accuracy))
 
 print("--- %s seconds ---" % (time() - start_time))
