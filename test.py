6570b91c02f240e7818d9e1e5255277d47f13808
Raphael Adamski <raphaeladamski@hotmail.com>
2018-06-05 00:07:02 -0300

    Improve testing, remove unnecessary code

--- test.py
+++ test.py
@@ -21,35 +21,6 @@ environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from joblib import Parallel, delayed
 import multiprocessing
 
-def resize(image, size=299, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    width = None
-    height = None
-    (h, w) = image.shape[:2]
-    if h > w:
-        width = size
-    elif h < w:
-        height = size
-    # check to see if the width is None
-    if height is not None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-    # otherwise, the height is None
-    elif width is not None:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-    else:
-        dim = (size, size)
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
-    # return the resized image
-    return resized
 
 start_time = time()
 parser = argparse.ArgumentParser(description='Dog breed classifier')
@@ -94,32 +65,31 @@ batch_size = 16
 test_datagen = preprocessing.image.ImageDataGenerator(
     rescale=1./255
 )
-
+batch_size = 1
 test_generator = test_datagen.flow_from_directory(
     TEST,
     classes=["{:02d}".format(x) for x in range(NUM_CLASSES)],
     target_size=(299, 299),
-    batch_size=1,
+    batch_size=batch_size,
     shuffle=False,
     class_mode='categorical'
 )
 test_generator.num_classes = 83
 
-model_name = "inceptionv3"
+model_name = "inceptionv3-aug"
 
 model = models.load_model(model_name + ".h5")
 print("Loaded: " + model_name)
 
-score = model.evaluate_generator(test_generator, verbose=2, steps=len(test_classes), use_multiprocessing=True)
+score = model.evaluate_generator(test_generator, verbose=2, steps=len(test_generator.filenames)//batch_size)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 test_generator.class_mode = None
 
-prob = model.predict_generator(test_generator, verbose=2, workers=1, steps=len(test_classes))
+prob = model.predict_generator(test_generator, verbose=2, steps=len(test_generator.filenames)//batch_size)
 
 Y_pred = np.argmax(prob, axis=1)
-print(Y_pred)
 accuracy = (len(test_classes) - np.count_nonzero(Y_pred - test_classes) + 0.0)/len(test_classes)
 print("Accuracy on test set of %d samples: %f" % (len(test_classes), accuracy))
 
